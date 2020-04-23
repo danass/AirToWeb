ba985b2c50967f6a185cf77b36e02052deac2635
Daniel Assayag <dseyag@gmail.com>
2020-04-23 08:59:28 +0200

    minor

--- src/index.js
+++ src/index.js
@@ -11,19 +11,7 @@ var UpdateData = require("./updateData.js");
 require('./villettemakerz')(site);
 
 const aws = process.platform != "win32"? true: false
-if(aws) {
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
-
-}
 
 const rootdir = process.cwd()
 console.log(rootdir)
@@ -36,6 +24,16 @@ httpServer.listen(8081, () => {
 
 
 if(aws) {
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/dc.villettemakerz.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 const httpsServer = https.createServer(credentials, site);
 httpsServer.listen(8082, () => {
 	console.log('HTTPS Server running on port 8082');
