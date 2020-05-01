3480cd585a3a2533645f1409b3967e95a1305d1a
Daniel Assayag <dseyag@gmail.com>
2020-05-01 17:12:16 +0200

    automated reading of data

--- src/databases.js
+++ src/databases.js
@@ -1,13 +1,35 @@
 const fs = require("fs");
+var MD5 = require("crypto-js/md5");
 
-const data = JSON.parse(fs.readFileSync("data/data.json", "utf8")); //Parse Source JSON des datas
-const dechetdata = JSON.parse(fs.readFileSync("data/dechetheque.json", "utf8")); //Parse Source JSON des datas
-const evaluation = JSON.parse(fs.readFileSync("data/evaluation.json", "utf8")); //Parse Source JSON des datas
+//declare all your data
+
+const rootdir = process.cwd()
+
+//get unique id for data file (for comparison)
+function md5(json) {
+  const jsonstr = JSON.stringify(json)
+  return MD5(jsonstr).toString()
+}
+
+//syncronous data reading
+var files = fs.readdirSync(rootdir + "/data")
+  database = {}
+  files.forEach(filename => {
+    let name = filename.split('.').slice(0, -1).join('.')
+    let jsontype = filename.split('.').pop() === "json" ? true : false
+    if(jsontype) {
+    database[name] = {} 
+    database[name].name = "data/" + filename
+    database[name].data = JSON.parse(fs.readFileSync("data/" + filename, "utf8"));
+    database[name].md5 = md5(database[name].data)
+    }
+  });
 
 exports.databases = {
-  data: data,
-  dechetdata: dechetdata,
-  evaluation: evaluation
+  db:database
 };
 
 module.exports = this.databases
+
+//show local export object
+//console.log(this.databases)
